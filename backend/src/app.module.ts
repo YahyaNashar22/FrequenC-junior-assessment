19c9ebd9139d5d807089c70dee237df4a1f32410
YahyaNashar22 <yahyanashar22@gmail.com>
2025-05-27 23:48:05 +0300

    added ConfigModule and established successfull db connection

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,20 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Connection } from 'mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URI ?? '', {
+      connectionFactory: (connection: Connection) => {
+        Logger.log('Successfully Connected to db');
+        return connection;
+      },
+    }),
+  ],
   controllers: [],
   providers: [],
 })
