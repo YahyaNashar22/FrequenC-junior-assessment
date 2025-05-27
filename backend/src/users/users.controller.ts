d8ce8d70dfaa976d7d25c045cf700e76e18c0421
YahyaNashar22 <yahyanashar22@gmail.com>
2025-05-28 01:45:18 +0300

    added and tested user controllers

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { RegisterDto } from './dto/register.dto';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private userService: UsersService) {}
+
+  @Post('register')
+  register(@Body() dto: RegisterDto) {
+    return this.userService.register(dto);
+  }
+
+  @Post('login')
+  login(@Body() dto: LoginDto) {
+    return this.userService.login(dto);
+  }
+}
